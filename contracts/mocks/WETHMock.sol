53e5a51165660c3376b435adcee709e3bc212786
Amirhossein Banavi <ahbanavi@gmail.com>
2022-04-23 16:34:47 +0430

    typo

--- contracts/mocks/WETHMock.sol
+++ contracts/mocks/WETHMock.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.13;
 
 import "@rari-capital/solmate/src/tokens/WETH.sol";
 
-/// @title Mock for testint ERC20 tokens functionallity
+/// @title Mock for testing ERC20 tokens functionallity
 /// @author Rtility (https://github.com/Rtility/)
 contract WETHMock is WETH {
 
